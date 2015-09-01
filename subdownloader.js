26addc0a9e221eb00c021bd8e27815a2b3371b80
beatfreaker <radiachints1512@gmail.com>
2015-09-01 09:13:50 +0530

    minor tweaks

--- subdownloader.js
+++ subdownloader.js
@@ -5,17 +5,33 @@ var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var Q = require('q');
-var progressBar = require('progress');
+var ProgressBar = require('progress');
 var videoExtensions = require('video-extensions');
 var chalk = require('chalk');
 var p = process.cwd();
 var extns = Object.create(null);
 var filesArray = [];
 var param = process.argv.slice(2);
-var deepString = "-deep";
-videoExtensions.forEach(function(el) {
+var deepString = '-deep';
+videoExtensions.forEach(function (el) {
 	el = extns[el] = true;
 });
+
+var filterFiles = function (files) {
+	try {
+		return files.filter(function (file) {
+			var returnMessage = false;
+			if (fs.statSync(file).isFile() && path.extname(file).slice(1).toLowerCase() in extns) {
+				returnMessage = true;
+			} else {
+				returnMessage = false;
+			}
+			return returnMessage;
+		});
+	} catch (err) {
+		console.log('Please check if all the file name given exists or not.');
+	}
+};
 
 var getFileList = function () {
 	var fileList;
@@ -25,16 +41,16 @@ var getFileList = function () {
 		if (err) {
 			throw err;
 		}
-		if(param.length === 0) {
+		if (param.length === 0) {
 			fileList = filterFiles(files);
 		} else {
 			fileList = filterFiles(param);
 		}
-		if(fileList) {
+		if (fileList) {
 			fileList.forEach(function (file) {
 				filesArray.push(file);
 				counter++;
-				if(counter > fileList.length) {
+				if (counter > fileList.length) {
 					defered.resolve(filesArray);
 				}
 			});
@@ -43,50 +59,67 @@ var getFileList = function () {
 	return defered.promise;
 };
 
-var filterFiles = function (files) {
-	try {
-		return files.filter(function (file) {
-			if(fs.statSync(file).isFile() && path.extname(file).slice(1).toLowerCase() in extns) {
-				return true;
-			}
-			else {
-				return false;
+var downloadSubtitle = function (fileName, hash) {
+	var defered = Q.defer();
+	var srtFileName = path.basename(fileName, path.extname(fileName)) + '.srt';
+	var srtFilePathName = path.dirname(fileName) + '/' + srtFileName;
+	var progressBarMessage = 'Downloading ' + chalk.bgBlue.bold(srtFileName) + ' [:bar] :percent :etas | :current of :total bytes';
+	var finalData = '';
+	var green = '\u001b[42m \u001b[0m';
+	var options = {
+		hostname: 'api.thesubdb.com',
+		path: '/?action=download&hash=' + hash + '&language=en',
+		method: 'GET',
+		headers: {'user-agent': 'SubDB/1.0'}
+	};
+	http.get(options, function (res) {
+		var len = parseInt(res.headers['content-length'], 10);
+		var bar = new ProgressBar(progressBarMessage, {
+			complete: green,
+			incomplete: '-',
+			width: 10,
+			total: len
+		});
+		res.statusCode = '' + (res.statusCode).toString();
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			bar.tick(chunk.length);
+			finalData += chunk;
+		});
+		res.on('end', function () {
+			if (res.statusCode === '200') {
+				fs.writeFile(srtFilePathName, finalData, function (err) {
+					if (err) {
+						console.log(err);
+					}
+					console.log('\n---------------------------------------------------------------------------------------------\n');
+					defered.resolve(fileName);
+				});
+			} else {
+				console.log('Sorry no subitles were found for ====> ' + chalk.bgRed.bold(fileName));
+				console.log('\n---------------------------------------------------------------------------------------------\n');
+				console.log();
+				defered.resolve('error');
 			}
 		});
-	} catch (err) {
-		console.log("Please check if all the file name given exists or not.");
-	}
-};
-
-var processFiles = function (fileList, index) {
-	return Q.resolve('val').then(function(value){
-		if(index == fileList.length) {
-			return 'done';
-		} else {
-			getHash(fileList[index]).then(function(d) {
-				return downloadSubtitle(fileList[index], d);
-			}).then(function(fileName) {
-				index++;
-				return processFiles(fileList, index);
-			});
-		}
 	});
+	return defered.promise;
 };
 
 var getHash = function (file) {
 	var defered = Q.defer();
 	var shasum = crypto.createHash('md5');
-	var s = fs.createReadStream(file, {start: 0, end: (64*1024)-1});
-	s.on('data', function(d) {
-	  shasum.update(d);
+	var s = fs.createReadStream(file, {start: 0, end: (64 * 1024) - 1});
+	s.on('data', function (d) {
+		shasum.update(d);
 	});
-	s.on('end', function() {
+	s.on('end', function () {
 		var stats = fs.statSync(file);
-		var sNew = fs.createReadStream(file, {start: stats.size - (64*1024), end: stats.size - 1});
-		sNew.on('data', function(d) {
-		  shasum.update(d);
+		var sNew = fs.createReadStream(file, {start: stats.size - (64 * 1024), end: stats.size - 1});
+		sNew.on('data', function (d) {
+			shasum.update(d);
 		});
-		sNew.on('end', function() {
+		sNew.on('end', function () {
 			var d = shasum.digest('hex');
 			defered.resolve(d);
 		});
@@ -94,71 +127,42 @@ var getHash = function (file) {
 	return defered.promise;
 };
 
-var downloadSubtitle = function (fileName, hash) {
-	var defered = Q.defer();
-	var srtFileName = path.basename(fileName, path.extname(fileName)) + '.srt';
-	var srtFilePathName = path.dirname(fileName) + "/" + srtFileName;
-	var progressBarMessage = 'Downloading ' + chalk.bgBlue.bold(srtFileName) + ' [:bar] :percent :etas | :current of :total bytes';
-	var finalData = '';
-	var green = '\u001b[42m \u001b[0m';
-	var options = {
-	  hostname: 'api.thesubdb.com',
-	  path:'/?action=download&hash='+hash+'&language=en',
-	  method: 'GET',
-	  headers: {'user-agent': 'SubDB/1.0'}
-	};
-	http.get(options, function(res) {
-		var len = parseInt(res.headers['content-length'], 10);
-		var bar = new progressBar(progressBarMessage, {
-			complete: green,
-			incomplete: '-',
-			width: 10,
-			total: len
-	  	});
-		res.statusCode = ''+res.statusCode;
-	  	res.setEncoding('utf8');
-	  	res.on('data', function (chunk) {
-	  		bar.tick(chunk.length);
-	  		finalData += chunk;
-	  	});
-	  	res.on('end',function(){
-	  		if(res.statusCode === '200') {
-	  			fs.writeFile(srtFilePathName, finalData,function(err){
-					if(err) {
-						console.log(err);
-					}
-					console.log('\n---------------------------------------------------------------------------------------------\n');
-					defered.resolve(fileName);
-				});
-	  		} else {
-	  			console.log('Sorry no subitles were found for ====> ' + chalk.bgRed.bold(fileName));
-	  			console.log('\n---------------------------------------------------------------------------------------------\n');
-	  			console.log();
-	  			defered.resolve("error");
-	  		}
-	  	});
+var processFiles = function (fileList, index) {
+	return Q.resolve('val').then(function () {
+		if (index === fileList.length) {
+			return 'done';
+		}
+		if (index <= fileList.length) {
+			getHash(fileList[index]).then(function (d) {
+				return downloadSubtitle(fileList[index], d);
+			}).then(function () {
+				index++;
+				return processFiles(fileList, index);
+			});
+		}
 	});
-	return defered.promise;
 };
 
-var getDeepFiles = function(currentDir, callback) {
-  fs.readdirSync(currentDir).forEach(function(name){
-    var filePath = path.join(currentDir, name);
-        var stat = fs.statSync(filePath);
-        if (stat.isFile() && path.extname(filePath).slice(1).toLowerCase() in extns) {
-            filesArray.push(filePath);
-        } else if (stat.isDirectory()) {
-            getDeepFiles(filePath);
-        }
-  });
+var getDeepFiles = function (currentDir) {
+	fs.readdirSync(currentDir).forEach(function (name) {
+		var filePath = path.join(currentDir, name);
+		var stat = fs.statSync(filePath);
+		if (stat.isFile() && path.extname(filePath).slice(1).toLowerCase() in extns) {
+			filesArray.push(filePath);
+		} else if (stat.isDirectory()) {
+			getDeepFiles(filePath);
+		}
+	});
 };
 
-var getPara = function() {
-	if(param.indexOf(deepString) > -1) {
+var getPara = function () {
+	if (param.indexOf(deepString) > -1) {
 		getDeepFiles(p);
-		processFiles(filesArray,0);
+		processFiles(filesArray, 0);
 	} else {
-		getFileList().then(function (data){processFiles(data,0);});
+		getFileList().then(function (data) {
+			processFiles(data, 0);
+		});
 	}
 };
 
