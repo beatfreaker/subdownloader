cddfcea3fa5d4d681aa1283b6c3868eaa512081b
beatfreaker <radiachints1512@gmail.com>
2015-09-22 00:38:19 +0530

    add test

---
+++ test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var assert = require('assert')
+var childProcess = require('child_process')
+
+describe('subdownloader', function () {
+	describe('file name check', function () {
+		it('not valid file name error', function () {
+			childProcess.execFile('./subdownloader.js', ['randomfilename'], {cwd: __dirname}, function (err, stdout) {
+				assert.equal(stdout, 'Please check if all the file name given exists or not.');
+			});
+		})
+	})
+})
