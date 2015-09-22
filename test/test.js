43a5d2b536584356be542df86a6b929bf72342ad
Chintan Radia <radiachints1512@gmail.com>
2015-09-22 10:40:54 +0530

    minor tweaks

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var assert = require('assert')
-var childProcess = require('child_process')
+var assert = require('assert');
+var childProcess = require('child_process');
 
 describe('subdownloader', function () {
 	describe('file name check', function () {
@@ -9,6 +9,6 @@ describe('subdownloader', function () {
 			childProcess.execFile('./subdownloader.js', ['randomfilename'], {cwd: __dirname}, function (err, stdout) {
 				assert.equal(stdout, 'Please check if all the file name given exists or not.');
 			});
-		})
-	})
-})
+		});
+	});
+});
