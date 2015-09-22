55298c1ca8578e8ac2e6e7bb07776b37536f0fa6
Chintan Radia <radiachints1512@gmail.com>
2015-09-22 11:14:20 +0530

    migrated to tape

--- test/test.js
+++ test/test.js
@@ -1,14 +1,10 @@
 'use strict';
-
-var assert = require('assert');
+var test = require('tape');
 var childProcess = require('child_process');
 
-describe('subdownloader', function () {
-	describe('file name check', function () {
-		it('not valid file name error', function () {
-			childProcess.execFile('./subdownloader.js', ['randomfilename'], {cwd: __dirname}, function (err, stdout) {
-				assert.equal(stdout, 'Please check if all the file name given exists or not.');
-			});
-		});
+test('Invalid file name', function (t) {
+	t.plan(1);
+	childProcess.execFile('../subdownloader.js', ['randomfilename'], {cwd: __dirname}, function (err, stdout, stderr) {
+		t.equal('Please check if all the file name given exists or not.\n', stdout, 'Both strings are equal');
 	});
 });
