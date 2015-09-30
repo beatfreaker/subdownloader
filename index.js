833b70d0d5d9166e219ef7a378700313baaafab1
beatfreaker <radiachints1512@gmail.com>
2015-09-30 22:16:11 +0530

    add api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 var Promise = require('pinkie-promise');
 var crypto = require('crypto');
 var fs = require('fs');
@@ -6,67 +6,42 @@ var path = require('path');
 var http = require('http');
 var ProgressBar = require('progress');
 var chalk = require('chalk');
-var arr = ['package.json','subdownloader.js'];
-
+var obj = {fileName: '', hash: ''};
+var returnObj = {success: [], failed: []};
 var exp = module.exports;
 
-exp.subdownload = function () {
-	return new Promise(function(resolve, reject) {
-		processFile(0).then(function(data){console.log('called');});	
-	});
-}
-
-var processFile = function (index) {
-	console.log("index :: " + index);
-	if(index == arr.length) {
-		console.log("returning");
-		return Promise(function(resolve, reject){
-			resolve('done');	
-		});
-	} else {
-		getHash(arr[index]).then(function(fileName, hash) {
-		    return downloadSubtitle(fileName, hash);
-		}).then(function(data) {
-		   processFile(++index);
-		});
-	}
-}
-
-
-var getHash = function (file) {
-	console.log("Generating Hash :: " + file);
+var getHash = function (obj) {
 	return new Promise(function (resolve, reject) {
 		var shasum = crypto.createHash('md5');
-		var s = fs.createReadStream(file, {start: 0, end: (64 * 1024) - 1});
+		var s = fs.createReadStream(obj.fileName, {start: 0, end: (64 * 1024) - 1});
 		s.on('data', function (d) {
 			shasum.update(d);
 		});
 		s.on('end', function () {
-			var stats = fs.statSync(file);
-			var sNew = fs.createReadStream(file, {start: stats.size - (64 * 1024), end: stats.size - 1});
+			var stats = fs.statSync(obj.fileName);
+			var sNew = fs.createReadStream(obj.fileName, {start: stats.size - (64 * 1024), end: stats.size - 1});
 			sNew.on('data', function (d) {
 				shasum.update(d);
 			});
 			sNew.on('end', function () {
 				var d = shasum.digest('hex');
-				resolve(file, d);
+				obj.hash = d;
+				resolve(obj);
 			});
 		});
 	});
 };
 
-
-var downloadSubtitle = function (fileName, hash) {
-	console.log("Downloading subtitle :: " + fileName);
+var downloadSubtitle = function (obj) {
 	return new Promise(function (resolve, reject) {
-		var srtFileName = path.basename(fileName, path.extname(fileName)) + '.srt';
-		var srtFilePathName = path.dirname(fileName) + '/' + srtFileName;
+		var srtFileName = path.basename(obj.fileName, path.extname(obj.fileName)) + '.srt';
+		var srtFilePathName = path.dirname(obj.fileName) + '/' + srtFileName;
 		var progressBarMessage = 'Downloading ' + chalk.bgBlue.bold(srtFileName) + ' [:bar] :percent :etas | :current of :total bytes';
 		var finalData = '';
 		var green = '\u001b[42m \u001b[0m';
 		var options = {
 			hostname: 'api.thesubdb.com',
-			path: '/?action=download&hash=' + hash + '&language=en',
+			path: '/?action=download&hash=' + obj.hash + '&language=en',
 			method: 'GET',
 			headers: {'user-agent': 'SubDB/1.0'}
 		};
@@ -90,24 +65,36 @@ var downloadSubtitle = function (fileName, hash) {
 						if (err) {
 							console.log(err);
 						}
-						console.log('\n---------------------------------------------------------------------------------------------\n');
-						resolve(fileName);
+						returnObj.success.push(obj.fileName);
+						resolve(obj);
 					});
 				} else {
-					console.log('Sorry no subitles were found for ====> ' + chalk.bgRed.bold(fileName));
-					console.log('\n---------------------------------------------------------------------------------------------\n');
-					console.log();
+					console.log('Sorry no subitles were found for ====> ' + chalk.bgRed.bold(obj.fileName));
+					returnObj.failed.push(obj.fileName);
 					resolve('error');
 				}
+				console.log('\n');
 			});
 		});
 	});
 };
 
+var processFiles = function (arr) {
+	return arr.reduce(function (promise, file) {
+		return promise.then(function () {
+			obj.fileName = file;
+			return getHash(obj).then(function (obj) {
+				return downloadSubtitle(obj);
+			});
+		});
+	}, Promise.resolve());
+};
 
-var tell = function (val) {
-	console.log("tell :: " + val);
+exp.subdownload = function (fileList, opt) {
+	opt = opt || {};
 	return new Promise(function (resolve, reject) {
-		setTimeout(function(){resolve('finished')}, 1000);	
+		processFiles(fileList).then(function () {
+			resolve(returnObj);
+		});
 	});
-}
+};
